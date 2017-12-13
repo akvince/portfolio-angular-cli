d558b1f8c5398426a5f6325cd4e2a32f595ed386
deflandre <deflandre.vincent@gmail.com>
2017-12-13 19:19:22 +0100

    correct styleUrls for sass

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-app-header',
   templateUrl: './app-header.component.html',
-  styleUrls: ['./app-header.component.css']
+  styleUrls: ['./app-header.component.sass']
 })
 export class AppHeaderComponent implements OnInit {
 
