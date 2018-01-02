44b9789f1408b9c173e394b759e928c85819f969
deflandre <deflandre.vincent@gmail.com>
2018-01-02 14:28:40 +0100

    create component cv

---
+++ src/app/curriculumvitae/curriculumvitae.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CurriculumvitaeComponent } from './curriculumvitae.component';
+
+describe('CurriculumvitaeComponent', () => {
+  let component: CurriculumvitaeComponent;
+  let fixture: ComponentFixture<CurriculumvitaeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CurriculumvitaeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CurriculumvitaeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/curriculumvitae/curriculumvitae.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curriculumvitae',
+  templateUrl: './curriculumvitae.component.html',
+  styleUrls: ['./curriculumvitae.component.css']
+})
+export class CurriculumvitaeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
