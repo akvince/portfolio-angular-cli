300727d7e559817cc531a19712144bb059845127
deflandre <deflandre.vincent@gmail.com>
2018-06-28 22:37:05 +0200

    add content presentation page

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./presentation.sass']
 })
 export class PresentationComponent implements OnInit {
-
+  presentation = {
+      content: 'Bonjour, alors moi c\'est Vincent Deflandre, j\'ai 27 ans. Je suis developpeur front end depuis 2016',
+      bonus: 'Aller faire un petit tour sur mon bac a sable',
+      linkSandbox: 'vincent-deflandre.ovh'
+  }
   constructor() { }
 
   ngOnInit() {
